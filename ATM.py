9051f68cf60e6f4e0dc1e10be06f2b8c01c86b74
V-gifs <78023473+V-gifs@users.noreply.github.com>
2021-04-03 21:58:31 +0200

    Added while loops so user doesn't have to rerun script

--- ATM.py
+++ ATM.py
@@ -1,51 +1,37 @@
 import datetime as dt
-name = input("What is your name? \n")
 allowedUsers = ['Seyi','Mike','Love']
 allowedPassword = ['passwordSeyi','passwordMike','passwordLove']
 today = dt.date.today()
-if(name in allowedUsers):
-    password = input("Your password? \n")
-    userId = allowedUsers.index(name)
-    if(password == allowedPassword[userId]):
-        print('Welcome %s' % name)
-        print(today.strftime("%B %d, %Y"))
-        print('these are the available options:')
-        print('1. Withdrawl')
-        print('2. Cash Deposit')
-        print('3. Complaint')
-        selectedOption = int(input('Please select an option:'))
-
-        if (selectedOption == 1):
-            withdraw = int(input('How much would you like to withdraw? \n'))
-            print('Take your cash!')
-            print('Welcome %s' % name)
-            print(today.strftime("%B %d, %Y"))
-            print('these are the available options:')
-            print('1. Withdrawl')
-            print('2. Cash Deposit')
-            print('3. Complaint')
-            selectedOption = int(input('Please select an option:'))
-        elif(selectedOption == 2):
-            deposit = int(input('How much would you like to deposit?'))
-            print('Current Balance: %d' % deposit)
-            print('Welcome %s' % name)
-            print(today.strftime("%B %d, %Y"))
-            print('these are the available options:')
-            print('1. Withdrawl')
-            print('2. Cash Deposit')
-            print('3. Complaint')
-        elif(selectedOption == 3):
-            complaint = input('What issue would you like to report?')
-            print('Thank you for contacting us')
-            print('Welcome %s' % name)
-            print(today.strftime("%B %d, %Y"))
-            print('these are the available options:')
-            print('1. Withdrawl')
-            print('2. Cash Deposit')
-            print('3. Complaint')
-        else:
-            print('Invalid Option selected, please try again')
-    else:
-        print('Password Incorrect, please try again')
-else:
+while True:
+    name = input("What is your name? \n")
+    if(name in allowedUsers):
+        while True:
+            password = input("Your password? \n")
+            userId = allowedUsers.index(name)
+            if(password == allowedPassword[userId]):
+                print('Welcome %s' % name)             
+                break
+            print('Wrong password')
+        break
     print('Name not found, please try again')
+flag = True
+while flag:
+    selectedOption = int(input('These are the available options: \n 1. Withdrawl \n 2. Cash Deposit \n 3. Complaint \n 4. Exit \n Please select an option:'))
+    if (selectedOption == 1):
+        withdrawl = int(input('How much would you like to withdraw? \n'))
+        print('Take your cash!')
+    elif (selectedOption == 2):
+        deposit = int(input('How much would you like to deposit? \n'))
+        print('Current Balance: %d' % deposit)
+    elif (selectedOption == 3):
+        complaint = input('What issue would you like to report? \n')
+        print('Thank you for contacting us.')
+    elif (selectedOption == 4):
+        print('Goodbye!')
+        flag = False
+    else:
+        print('Invalid Option selected, please try again.')
+    
+
+    
+                
